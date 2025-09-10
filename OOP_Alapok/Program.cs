614a0fa494d316f903515181c8a91d5a0cae8328
vecseizs <vecseizs@kkszki.hu>
2025-09-10 09:24:54 +0200

    Feladat 3 is ready

--- OOP_Alapok/Program.cs
+++ OOP_Alapok/Program.cs
@@ -11,6 +11,20 @@ namespace OOP_Alapok
         string nev;
         int kor;
 
+
+        public string Nev
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+
+        public int Kor
+        {
+            get { return kor; }
+            set { kor = value; }
+        }
+
+        /*
         public Szemely(string Nev, int Kor)
         {
             nev = Nev;
@@ -21,17 +35,25 @@ namespace OOP_Alapok
         {
             return $"A tanuló neve: {nev} és {kor} éves.";
         }
+        */
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            Szemely tanulo1 = new Szemely();
+            tanulo1.Nev = "Gábor";
+            tanulo1.Kor = 26;
+            Console.WriteLine(tanulo1.Nev + ", " + tanulo1.Kor);
+
+            /*
             Szemely tanulo1 = new Szemely("Peti", 34);
             Console.WriteLine(tanulo1.Kiir());
 
             Szemely tanulo2 = new Szemely("Jani", 38);
             Console.WriteLine(tanulo2.Kiir());
+            */
         }
     }
 }
